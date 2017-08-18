44cde1e00e95d48e30dfbf3dcd0d7430b107a423
CrazyBunQnQ <baobao222222@qq.com>
2017-08-18 17:59:57 +0800

    update: add two faster solution.

--- src/main/java/com/crazybunqnq/leetcode/database/easy/EmployeesEarningMoreThanTheirManagers.sql
+++ src/main/java/com/crazybunqnq/leetcode/database/easy/EmployeesEarningMoreThanTheirManagers.sql
@@ -24,5 +24,15 @@ insert into Employee (Id, Name, Salary, ManagerId) values ('2', 'Henry', '80000'
 insert into Employee (Id, Name, Salary, ManagerId) values ('3', 'Sam', '60000', 'None');
 insert into Employee (Id, Name, Salary, ManagerId) values ('4', 'Max', '90000', 'None');
 
-# My SQL Statement
+# My SQL Statement 2274ms
 SELECT e.Name Employee FROM Employee e WHERE e.Salary > (SELECT salary FROM Employee c WHERE c.Id = e.ManagerId);
+
+# 1952ms
+select E1.Name Employee
+from Employee as E1, Employee as E2
+where E1.ManagerId = E2.Id and E1.Salary > E2.Salary
+
+# 1746ms
+select a.Name Employee
+from Employee a inner join Employee b on a.ManagerId=b.Id
+where a.Salary>b.Salary
