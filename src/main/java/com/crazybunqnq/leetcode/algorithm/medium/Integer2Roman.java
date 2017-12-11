d5425bf17382eba401f496f6c18fa9c89a1e3190
CrazyBunQnQ <baobao222222@qq.com>
2017-12-11 23:56:49 +0800

    完成第一道中级题！哈哈哈哈，但是看到别人的程序下一条，一个 if 都没有用到...

---
+++ src/main/java/com/crazybunqnq/leetcode/algorithm/medium/Integer2Roman.java
@@ -0,0 +1,92 @@
+package com.crazybunqnq.leetcode.algorithm.medium;
+
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an integer, convert it to a roman numeral.
+ * <p>
+ * Input is guaranteed to be within the range from 1 to 3999.
+ *
+ * @version 2017/7/18.
+ * @auther CrazyBunQnQ
+ */
+@Log4j
+public class Integer2Roman {
+
+    @Test
+    public void test() {
+        log.info(intToRoman(1894));
+        log.info(intToRoman95ms(9));
+        log.info(intToRoman95ms(1884));
+    }
+
+    public String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] strs = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                sb.append(strs[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+    public String intToRoman95ms(int num) {
+        Map<String, Integer> romanMap = new HashMap<String, Integer>();
+        romanMap.put("M", 1000);
+        romanMap.put("D", 500);
+        romanMap.put("C", 100);
+        romanMap.put("L", 50);
+        romanMap.put("X", 10);
+        romanMap.put("V", 5);
+        romanMap.put("I", 1);
+        StringBuffer sb = new StringBuffer();
+        String[] roman = {"M", "D", "C", "L", "X", "V", "I"};
+
+        while (num > 0) {
+            for (int i = 0; i < roman.length; i++) {
+                String str = roman[i];
+                int num1 = romanMap.get(str);
+                if (num / num1 > 0) {
+                    sb.append(str);
+                    num -= num1;
+                    i--;
+                } else if (i + 1 < roman.length && isCXI(roman[i + 1])) {
+                    String str2 = roman[i + 1];
+                    int num2 = romanMap.get(str2);
+                    if ((num + num2) / num1 > 0) {
+                        sb.append(str2 + str);
+                        num -= (num1 - num2);
+                    }
+                } else if (i + 2 < roman.length && isCXI(roman[i + 2])) {
+                    String str2 = roman[i + 2];
+                    int num2 = romanMap.get(str2);
+                    if ((num + num2) / num1 > 0) {
+                        sb.append(str2 + str);
+                        num -= (num1 - num2);
+                    }
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 判断罗马数字字符是否为 C X I 之一
+     *
+     * @param s
+     *
+     * @return
+     */
+    public boolean isCXI(String s) {
+        return "I".equals(s) || "X".equals(s) || "C".equals(s);
+    }
+}
