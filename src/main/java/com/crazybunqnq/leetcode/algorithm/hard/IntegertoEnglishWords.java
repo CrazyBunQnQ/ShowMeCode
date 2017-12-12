b1cb1ca8d5d11a9d457a903490b0f9546e25c422
CrazyBunQnQ <baobao222222@qq.com>
2017-12-12 21:40:48 +0800

    优化代码，更简洁

--- src/main/java/com/crazybunqnq/leetcode/algorithm/hard/IntegertoEnglishWords.java
+++ src/main/java/com/crazybunqnq/leetcode/algorithm/hard/IntegertoEnglishWords.java
@@ -3,9 +3,6 @@ package com.crazybunqnq.leetcode.algorithm.hard;
 import lombok.extern.log4j.Log4j;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Convert a non-negative integer to its english words representation. Given input is guaranteed to be less than 2^31 - 1.
  * <p>
@@ -20,11 +17,14 @@ import java.util.Map;
  */
 @Log4j
 public class IntegertoEnglishWords {
+    private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
     private String[] oneToNine = {"One ", "Two ", "Three ", "Four ", "Five ", "Six ", "Seven ", "Eight ", "Nine "};
     private String[] twentyToNinety = {"Twenty ", "Thirty ", "Forty ", "Fifty ", "Sixty ", "Seventy ", "Eighty ", "Ninety "};
     private String[] elevenToNineteen = {"Ten ", "Eleven ", "Twelve ", "Thirteen ", "Fourteen ", "Fifteen ", "Sixteen ", "Seventeen ", "Eighteen ", "Nineteen "};
 
-    private String numberToWords(int num) {
+    private String numberToWords5ms(int num) {
         StringBuilder sb = new StringBuilder();
         if (num == 0) {
             return "Zero";
@@ -53,17 +53,19 @@ public class IntegertoEnglishWords {
 
     @Test
     public void mainTest() {
-        log.info(numberToWords(100));
+        log.info(numberToWords5ms(100));
         int i = 0;
         while (i <= 2147483647) {
-            log.info(i + ": " + numberToWords(i));
+            log.info(i + ": " + numberToWords5ms(i));
             i++;
         }
     }
 
     /**
      * 输出数字 1~999 的英文
+     *
      * @param num 1~999 的数字
+     *
      * @return 英文
      */
     private String oneToThousandStr(int num) {
@@ -85,5 +87,33 @@ public class IntegertoEnglishWords {
             }
         }
         return sb.toString();
+    }
+
+
+    public String numberToWords(int num) {
+        if (num == 0) return "Zero";
+
+        int i = 0;
+        String words = "";
+
+        while (num > 0) {
+            if (num % 1000 != 0)
+                words = helper(num % 1000) + THOUSANDS[i] + " " + words;
+            num /= 1000;
+            i++;
+        }
+
+        return words.trim();
+    }
+
+    private String helper(int num) {
+        if (num == 0)
+            return "";
+        else if (num < 20)
+            return LESS_THAN_20[num] + " ";
+        else if (num < 100)
+            return TENS[num / 10] + " " + helper(num % 10);
+        else
+            return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
     }
 }
