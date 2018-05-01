8e948b883ddacecf1daa8aeb5f3e70071db2bb55
CrazyBunQnQ <baobao222222@qq.com>
2018-05-01 11:41:30 +0800

    优化代码

--- src/main/java/com/crazybunqnq/designpattern/factory_simple/Shape.java
+++ src/main/java/com/crazybunqnq/designpattern/factory_simple/Shape.java
@@ -5,5 +5,9 @@ package com.crazybunqnq.designpattern.factory_simple;
  * @auther CrazyBunQnQ
  */
 public interface Shape {
+    public static final String CIRCLE = "CIRCLE";
+    public static final String RECTANGLE = "RECTANGLE";
+    public static final String SQUARE = "SQUARE";
+
     void draw();
 }
--- src/main/java/com/crazybunqnq/designpattern/factory_simple/ShapeFactory.java
+++ src/main/java/com/crazybunqnq/designpattern/factory_simple/ShapeFactory.java
@@ -5,20 +5,17 @@ package com.crazybunqnq.designpattern.factory_simple;
  * @auther CrazyBunQnQ
  */
 public class ShapeFactory {
-    public static final String CIRCLE = "CIRCLE";
-    public static final String RECTANGLE = "RECTANGLE";
-    public static final String SQUARE = "SQUARE";
 
     public Shape getShape(String shapeType) {
         if (shapeType == null || "".equals(shapeType)) {
             return null;
         }
         switch (shapeType.toUpperCase()) {
-            case CIRCLE:
+            case Shape.CIRCLE:
                 return new Circle();
-            case RECTANGLE:
+            case Shape.RECTANGLE:
                 return new Rectangle();
-            case SQUARE:
+            case Shape.SQUARE:
                 return new Square();
             default:
                 return null;
