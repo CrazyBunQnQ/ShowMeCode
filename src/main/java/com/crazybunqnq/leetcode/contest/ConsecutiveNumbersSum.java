eb7e88079aa2a1a08c11779a1402b0c125cbe406
CrazyBunQnQ <baobao222222@qq.com>
2018-05-06 22:26:37 +0800

    How can I get a little faster?

--- src/main/java/com/crazybunqnq/leetcode/contest/ConsecutiveNumbersSum.java
+++ src/main/java/com/crazybunqnq/leetcode/contest/ConsecutiveNumbersSum.java
@@ -1,5 +1,6 @@
 package com.crazybunqnq.leetcode.contest;
 
+import com.crazybunqnq.util.TimeUtil;
 import org.junit.Test;
 
 /**
@@ -28,63 +29,86 @@ import org.junit.Test;
  */
 public class ConsecutiveNumbersSum {
     /**
-     * Sn = (a1 + an) * n / 2
-     * d = (an - a1) / (n - 1)
+     * Sn = (a1 + an) * n / 2 = N
+     * <p>
+     * a1 + an = Sn * 2 / n 整数
+     * <p>
+     * d = (an - a1) / (n - 1) = 1
      *
      * @param N
      *
      * @return
      */
     public int consecutiveNumbersSum(int N) {
-        int result = 1;
-        Integer n = N / 2 + 1;
-        int sum;
+        int result = 1;//结果
+        int n = 2;//项数
+        int a1;//首项
+        int an;//末项
+        int d;//公差
+        int sum;//和
 
-        while (n > 1 && N != 2) {
+        while (n <= Math.ceil((double) N / 2)) {
+            // 首项加末项一定是整数
             if (N * 2 % n != 0) {
-                n--;
+                n++;
                 continue;
             }
-            int am = N / n;
+            int am = N / n;//是中间数
             if (N % n == 0) {
-                //N/n 是中间数
-                int m = (n - 1) / 2;
-                int a1 = am - m;
-                if (a1 <= 0) {
-                    n--;
+                int m = (n - 1) / 2;//中间数下标
+                if (m == 0) {//下标必须是整数
+                    n++;
                     continue;
                 }
-                int an = am + m;
-                sum = (a1 + an) * n / 2;
+                a1 = am - m;
+                if (a1 <= 0) {
+                    n++;
+                    continue;
+                }
+                an = am + m;
             } else {
                 int m1 = n / 2 - 1;
-                int m2 = n / 2;
-                int a1 = am - m1;
+                a1 = am - m1;
                 if (a1 <= 0) {
-                    n--;
+                    n++;
                     continue;
                 }
-                int an = am + 1 + m1;
-                sum = (a1 + an) * n / 2;
+                an = am + 1 + m1;
             }
-            n--;
-            if (N == sum) {
+            sum = (a1 + an) * n / 2;
+            d = (an - a1) / (n - 1);
+            n++;
+            if (N == sum && d == 1) {
                 result++;
             }
         }
-
         return result;
     }
 
     @Test
     public void test() {
-//        System.out.println(consecutiveNumbersSum(1));//1
-        System.out.println(consecutiveNumbersSum(2));//1
-//        System.out.println(consecutiveNumbersSum(3));//2
-        System.out.println(consecutiveNumbersSum(4));//1
-//        System.out.println(consecutiveNumbersSum(5));//2
-        System.out.println(consecutiveNumbersSum(6));//2
-//        System.out.println(consecutiveNumbersSum(15));//4
-//        System.out.println(consecutiveNumbersSum((int) Math.pow(10, 9)));
+        for (int i = 1; i <= Math.pow(10, 9); i = i + (int) (Math.random() * 100)) {
+            TimeUtil.setCurrentTime();
+            System.out.println("N = " + i + ": " + consecutiveNumbersSum(i) + "个，耗时 " + TimeUtil.runTime() + " 毫秒");
+        }
+        /*System.out.println("N = 2: " + consecutiveNumbersSum(1) + "；应该是 1");
+        System.out.println("N = 2: " + consecutiveNumbersSum(2) + "；应该是 1");
+        System.out.println("N = 3: " + consecutiveNumbersSum(3) + "；应该是 2");
+        System.out.println("N = 4: " + consecutiveNumbersSum(4) + "；应该是 1");
+        System.out.println("N = 5: " + consecutiveNumbersSum(5) + "；应该是 2");
+        System.out.println("N = 6: " + consecutiveNumbersSum(6) + "；应该是 2");
+        System.out.println("N = 7: " + consecutiveNumbersSum(7) + "；应该是 2");
+        System.out.println("N = 8: " + consecutiveNumbersSum(8) + "；应该是 1");
+        System.out.println("N = 9: " + consecutiveNumbersSum(9) + "；应该是 3");
+        System.out.println("N = 10: " + consecutiveNumbersSum(10) + "；应该是 2");
+        System.out.println("N = 11: " + consecutiveNumbersSum(11) + "；应该是 2");
+        System.out.println("N = 12: " + consecutiveNumbersSum(12) + "；应该是 2");
+        System.out.println("N = 13: " + consecutiveNumbersSum(13) + "；应该是 2");
+        System.out.println("N = 14: " + consecutiveNumbersSum(14) + "；应该是 2");
+        System.out.println("N = 15: " + consecutiveNumbersSum(15) + "；应该是 4");
+        System.out.println("N = 16: " + consecutiveNumbersSum(16) + "；应该是 2");
+        System.out.println("N = 17: " + consecutiveNumbersSum(17) + "；应该是 2");
+        System.out.println("N = 18: " + consecutiveNumbersSum(18) + "；应该是 2");
+        System.out.println("N = 19: " + consecutiveNumbersSum(19) + "；应该是 2");*/
     }
 }
