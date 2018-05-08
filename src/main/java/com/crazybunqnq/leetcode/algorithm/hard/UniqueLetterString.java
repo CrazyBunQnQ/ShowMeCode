55d8fc59a613f846efb69acbd0722579255aa207
CrazyBunQnQ <baobao222222@qq.com>
2018-05-08 22:38:06 +0800

    gets the letters that are not repeated.

---
+++ src/main/java/com/crazybunqnq/leetcode/algorithm/hard/UniqueLetterString.java
@@ -0,0 +1,71 @@
+package com.crazybunqnq.leetcode.algorithm.hard;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A character is unique in string S if it occurs exactly once in it.
+ * <p>
+ * For example, in string S = "LETTER", the only unique characters are "L" and "R".
+ * <p>
+ * Let's define UNIQ(S) as the number of unique characters in string S.
+ * <p>
+ * For example, UNIQ("LETTER") =  2.
+ * <p>
+ * Given a string S, calculate the sum of UNIQ(substring) over all non-empty substrings of S.
+ * <p>
+ * If there are two or more equal substrings at different positions in S, we consider them different.
+ * <p>
+ * Since the answer can be very large, retrun the answer modulo 10 ^ 9 + 7.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "ABC"
+ * Output: 10
+ * Explanation: All possible substrings are: "A","B","C","AB","BC" and "ABC".
+ * Evey substring is composed with only unique letters.
+ * Sum of lengths of all substring is 1 + 1 + 1 + 2 + 2 + 3 = 10
+ * Example 2:
+ * <p>
+ * Input: "ABA"
+ * Output: 8
+ * Explanation: The same as example 1, except uni("ABA") = 1.
+ * <p>
+ * <p>
+ * Note: 0 <= S.length <= 10000.
+ *
+ * @version 2018/5/6.
+ * @Score 9
+ * @auther CrazyBunQnQ
+ */
+public class UniqueLetterString {
+    public int uniqueLetterString(String S) {
+        int result = 0;
+        // find repeat chars
+        int size = 0;
+        StringBuilder sb = new StringBuilder();
+        Set<Character> subChars = new HashSet<Character>();
+        // Set<Character> dittogram = new HashSet<Character>();
+        for (int i = 0; i < S.length(); i++) {
+            Character c = S.charAt(i);
+            subChars.add(c);
+            if (subChars.size() == size) {
+                if (sb.indexOf(c + "") == -1)
+                    sb.append(sb.length() == 0 ? "[" + c : c);
+            } else {
+                size++;
+            }
+        }
+        if (sb.length() > 0) {
+            sb.append("]+");
+            String rex = sb.toString();
+            S = S.replaceAll(rex, "-");
+        }
+        System.out.println(S);
+
+
+        return result;
+    }
+}
