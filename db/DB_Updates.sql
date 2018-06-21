16cac26a706fb2b9d19df68e47877baa0f067f88
CrazyBunQnQ <baobao222222@qq.com>
2018-06-21 10:02:13 +0800

    Add comments

--- db/DB_Updates.sql
+++ db/DB_Updates.sql
@@ -1,17 +1,19 @@
+# Please add updates at the end in order
+# 2018-06-21
 CREATE TABLE LeetCode.Address
 (
-    AddressId int(11),
-    PersonId int(11),
-    City varchar(255),
-    State varchar(255)
+  AddressId int(11),
+  PersonId  int(11),
+  City      varchar(255),
+  State     varchar(255)
 );
 INSERT INTO LeetCode.Address (AddressId, PersonId, City, State) VALUES (1, 2, 'New York City', 'New York');
 CREATE TABLE LeetCode.Employee
 (
-    Id int(11),
-    Name varchar(255),
-    Salary int(11),
-    ManagerId int(11)
+  Id        int(11),
+  Name      varchar(255),
+  Salary    int(11),
+  ManagerId int(11)
 );
 INSERT INTO LeetCode.Employee (Id, Name, Salary, ManagerId) VALUES (1, 'Joe', 70000, 3);
 INSERT INTO LeetCode.Employee (Id, Name, Salary, ManagerId) VALUES (2, 'Henry', 80000, 4);
@@ -19,17 +21,17 @@ INSERT INTO LeetCode.Employee (Id, Name, Salary, ManagerId) VALUES (3, 'Sam', 60
 INSERT INTO LeetCode.Employee (Id, Name, Salary, ManagerId) VALUES (4, 'Max', 90000, 0);
 CREATE TABLE LeetCode.Person
 (
-    PersonId int(11),
-    FirstName varchar(255),
-    LastName varchar(255)
+  PersonId  int(11),
+  FirstName varchar(255),
+  LastName  varchar(255)
 );
 INSERT INTO LeetCode.Person (PersonId, FirstName, LastName) VALUES (1, 'Allen', 'Wang');
 CREATE TABLE LeetCode.salary
 (
-    id int(11),
-    name varchar(100),
-    sex char(1),
-    salary int(11)
+  id     int(11),
+  name   varchar(100),
+  sex    char(1),
+  salary int(11)
 );
 INSERT INTO LeetCode.salary (id, name, sex, salary) VALUES (1, 'A', 'f', 2500);
 INSERT INTO LeetCode.salary (id, name, sex, salary) VALUES (2, 'B', 'm', 1500);
