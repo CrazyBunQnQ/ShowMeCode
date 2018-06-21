3f8221f572271022e9638c3e8052fb8dcba1ff67
CrazyBunQnQ <baobao222222@qq.com>
2018-06-21 09:51:36 +0800

    Add methods to filter Emoji

--- src/main/java/com/crazybunqnq/util/StringUtil.java
+++ src/main/java/com/crazybunqnq/util/StringUtil.java
@@ -26,4 +26,50 @@ public class StringUtil {
         }
         return new String(word);
     }
+
+    /**
+     * 判断是否是 Emoji 字符
+     *
+     * @param codePoint
+     *
+     * @return
+     */
+    private boolean isEmojiCharacter(char codePoint) {
+        return (codePoint == 0x0) || (codePoint == 0x9) || (codePoint == 0xA)
+                || (codePoint == 0xD)
+                || ((codePoint >= 0x20) && (codePoint <= 0xD7FF))
+                || ((codePoint >= 0xE000) && (codePoint <= 0xFFFD))
+                || ((codePoint >= 0x10000) && (codePoint <= 0x10FFFF));
+    }
+
+    /**
+     * 过滤 Emoji 或者其他非文字类型（4个字节）的字符
+     *
+     * @param source
+     *
+     * @return
+     */
+    public String filterEmoji(String source) {
+        StringBuilder buf = null;
+        int len = source.length();
+        for (int i = 0; i < len; i++) {
+            char codePoint = source.charAt(i);
+            if (isEmojiCharacter(codePoint)) {
+                if (buf == null) {
+                    buf = new StringBuilder(source.length());
+                }
+                buf.append(codePoint);
+            }
+        }
+        if (buf == null) {
+            return source;
+        } else {
+            if (buf.length() == len) {
+                buf = null;
+                return source;
+            } else {
+                return buf.toString();
+            }
+        }
+    }
 }
