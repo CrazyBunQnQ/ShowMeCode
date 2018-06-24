7daf70ba6ce27959543dce401d31d0ab578168d0
CrazyBunQnQ <baobao222222@qq.com>
2018-06-24 09:37:51 +0800

    try~

---
+++ src/main/java/com/crazybunqnq/leetcode/algorithm/easy/BuddyStrings.java
@@ -0,0 +1,63 @@
+package com.crazybunqnq.leetcode.algorithm.easy;
+
+import org.junit.Test;
+
+/**
+ * Given two strings A and B of lowercase letters, return true if and only if we can swap two letters in A so that the result equals B.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: A = "ab", B = "ba"
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: A = "ab", B = "ab"
+ * Output: false
+ * Example 3:
+ * <p>
+ * Input: A = "aa", B = "aa"
+ * Output: true
+ * Example 4:
+ * <p>
+ * Input: A = "aaaaaaabc", B = "aaaaaaacb"
+ * Output: true
+ * Example 5:
+ * <p>
+ * Input: A = "", B = "aa"
+ * Output: false
+ * <p>
+ * <p>
+ * Note:
+ * <p>
+ * 0 <= A.length <= 20000
+ * 0 <= B.length <= 20000
+ * A and B consist only of lowercase letters.
+ *
+ * @version 2018/6/24.
+ * @Score 4
+ * @auther CrazyBunQnQ
+ */
+public class BuddyStrings {
+    public boolean buddyStrings(String A, String B) {
+        if (A.length() != B.length())
+            return false;
+        int n = 0;
+        for (int i = 0; i < A.length(); i++) {
+            if (A.charAt(i) != B.charAt(i)) {
+                n++;
+                if (n > 2)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    @Test
+    public void test() {
+        String[] As = {"ab", "ab", "aa", "aaaaaaabc", ""};
+        String[] Bs = {"ba", "ab", "aa", "aaaaaaacb", "aa"};
+//        for ()
+    }
+}
