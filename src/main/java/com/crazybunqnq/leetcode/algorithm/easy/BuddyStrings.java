e4afd17ce2d5edfc25cb7fc84e1e4745d421f055
CrazyBunQnQ <baobao222222@qq.com>
2018-06-24 10:11:16 +0800

    .....

--- src/main/java/com/crazybunqnq/leetcode/algorithm/easy/BuddyStrings.java
+++ src/main/java/com/crazybunqnq/leetcode/algorithm/easy/BuddyStrings.java
@@ -40,27 +40,52 @@ import org.junit.Test;
  * @auther CrazyBunQnQ
  */
 public class BuddyStrings {
+    /**
+     * 判断包含重复字母
+     *
+     * @param str
+     *
+     * @return
+     */
+    public boolean hasRepeatedLetters(String str) {
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+        char[] elements = str.toCharArray();
+        for (char e : elements) {
+            if (str.indexOf(e) != str.lastIndexOf(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public boolean buddyStrings(String A, String B) {
         if (A.length() != B.length() || A.length() < 2)
             return false;
-        if (A.length() == 2 && A.equals(B)) {
+        if (A.equals(B) && !hasRepeatedLetters(A))
             return false;
-        }
-        int n = 0;
+        if (A.length() == 2 && A.charAt(0) == B.charAt(1) && A.charAt(1) == B.charAt(0))
+            return true;
+        int n = 0, sumA = 0, sumB = 0;
         for (int i = 0; i < A.length(); i++) {
+            sumA += A.charAt(i);
+            sumB += B.charAt(i);
             if (A.charAt(i) != B.charAt(i)) {
                 n++;
-                if ((A.length() == 3 && n > 1) || A.length() > 3 && n > 2)
-                    return false;
             }
         }
+        if (sumA != sumB)
+            return false;
+        if ((A.length() == 2 && n != 2) || (A.length() == 3 && n > 1) || A.length() > 3 && n > 2)
+            return false;
         return true;
     }
 
     @Test
     public void test() {
-        String[] As = {"ab", "ab", "aa", "aaaaaaabc", ""};
-        String[] Bs = {"ba", "ab", "aa", "aaaaaaacb", "aa"};
+        String[] As = {"ab", "ab", "aa", "aaaaaaabc", "", "abcd", "ab"};
+        String[] Bs = {"ba", "ab", "aa", "aaaaaaacb", "aa", "abcd", "ca"};
         for (int i = 0; i < As.length; i++) {
             System.out.println(buddyStrings(As[i], Bs[i]));
         }
